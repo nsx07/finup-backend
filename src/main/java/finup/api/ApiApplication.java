69373083e00a2b2fcf04959b39bd3fa62e87d736
felipe <felipeaugustodluz@hotmail.com>
2023-08-21 19:22:31 -0300

    Adicionando pom.xml

--- src/main/java/finup/api/ApiApplication.java
+++ src/main/java/finup/api/ApiApplication.java
@@ -3,12 +3,15 @@ package finup.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-		System.out.println("[API - ON]");
+		System.out.print(LocalDateTime.now());
+		System.out.println(" -- [API - ON]");
 	}
 
 }
