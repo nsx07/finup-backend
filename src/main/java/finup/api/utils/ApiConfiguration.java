335ccaea02dc3128df617fe687d823190d731623
nsx07 <felipeaugustodluz@hotmail.com>
2023-11-04 16:32:44 -0300

    adicionando swagger-doc

--- src/main/java/finup/api/utils/ApiConfiguration.java
+++ src/main/java/finup/api/utils/ApiConfiguration.java
@@ -1,7 +1,31 @@
 package finup.api.utils;
 
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class ApiConfiguration {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Finup - Rest API")
+                        .description("API ")
+                        .version("1.0")
+                        .termsOfService("Termo de uso: Open Source")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.seusite.com.br")
+                        )
+                ).externalDocs(
+                        new ExternalDocumentation()
+                                .description("Seu nome")
+                                .url("http://www.seusite.com.br"));
+    }
 }
--- src/main/java/finup/api/utils/WebConfig.java
+++ src/main/java/finup/api/utils/WebConfig.java
@@ -1,9 +1,11 @@
 package finup.api.utils;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 @EnableWebMvc
@@ -13,4 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+
 }
